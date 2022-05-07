e712521b2d38467c7b8f5ed6a5e153ffbdb03ca6
Danisve <102831644+Danisve@users.noreply.github.com>
2022-05-07 10:45:02 -0600

    IMAGEN

--- efecto.py
+++ efecto.py
@@ -2,4 +2,4 @@ import streamlit as st
 st.title("Efecto fotoélectrico")
 st.write("Cuando la luz brilla en un metal,los electrones pueden ser expulsados de la superficie del metal en un fenómeno conocido como el efecto fotoeléctrico.")
 st.write("Debido al efecto fotoeléctrico fue prueba que la luz tiene una dualidad, es decir, se puede comportar como partícula y como onda.")
-st.image("foto1.jpg")
+st.image("foto1.PNG")
