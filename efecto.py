d04f6ac674814333bedfb0ccc6e2f3571c651690
Danisve <102831644+Danisve@users.noreply.github.com>
2022-05-07 11:41:24 -0600

    formula

--- efecto.py
+++ efecto.py
@@ -5,6 +5,7 @@ st.write("Debido al efecto fotoeléctrico fue prueba que la luz tiene una dualid
 st.image("FOTO.png")
 st.subheader("Modelo propuesto por Einstein")
 st.write("Con base en el modelo ondulatorio de la luz, los físicos predijeron que el aumento de la amplitud de la luz incrementaría la energía cinética de los fotoelectrones emitidos, mientras que el aumento de la frecuencia incrementaría la corriente medida. Contrario a las predicciones, los experimentos mostraron que el aumento en la frecuencia incrementaba la energía cinética de los fotoelectrones, mientras que el aumento en la amplitud de la luz incrementaba la corriente.")
-st.write("Con base en estos descubrimientos, Einstein propuso que la luz se comportaba como una corriente de partículas llamadas fotones con una energía de \text{E}=h\nuE=hνstart text, E, end text, equals, h, \nu.")
+st.write("Con base en estos descubrimientos, Einstein propuso que la luz se comportaba como una corriente de partículas llamadas fotones con una energía de:")
+st.latex("E= hv")
 st.markdown("![Alt Text](https://upload.wikimedia.org/wikipedia/commons/6/6e/EFECTOFOTOELECTRICOpeq.gif)")
 st.subheader("Bandgap")
